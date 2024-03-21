002c721e58f0b23f4be8f91c716a960a7da3ac5c
Jleal60 <134532990+Jleal60@users.noreply.github.com>
2024-03-21 12:54:36 +0000

    3/14

--- Project.js
+++ Project.js
@@ -1,12 +1,18 @@
 let pic1 = document.querySelector(".pic1");
+let pic1Button = document.querySelector(".pic1");
 let pic2 = document.querySelector(".pic2");
+let pic2Button = document.querySelector(".pic1");
 let pic3 = document.querySelector(".pic3");
+let pic3Button = document.querySelector(".pic1");
 let pic4 = document.querySelector(".pic4");
+let pic4Button = document.querySelector(".pic1");
 let pic5 = document.querySelector(".pic5");
+let pic5Button = document.querySelector(".pic1");
 let pic6 = document.querySelector(".pic6");
+let pic6Button = document.querySelector(".pic1");
 let pic7 = document.querySelector(".pic7");
+let pic7Button = document.querySelector(".pic1");
 
-pic1.addEventListener("click", () => {
-
-
-});
+pic2Button.addEventListener("click", function() {
+    pic2.style.display = "block";
+  });
